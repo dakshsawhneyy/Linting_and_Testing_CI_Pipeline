2023d67f005152a0e083409f6635502e879219a9
Daksh Sawhney <darkstardaksh@gmail.com>
2026-03-22 11:33:26 +0000

    Calculator application finished

---
+++ app.py
@@ -0,0 +1,20 @@
+# Implement Calculator Application
+
+# Addition Function
+def add(a, b):
+    return a + b
+
+
+# Subtraction Function
+def subtract(a, b):
+    return a - b
+
+
+# Multiply Function
+def multiply(a, b):
+    return a * b
+
+
+# Divide Function
+def divide(a, b):
+    return a / b
